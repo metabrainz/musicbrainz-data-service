7792ffc9cb2215de671b5f2c6d096aa2577b1aec
Oliver Charles <oliver.g.charles@googlemail.com>
2012-10-17 17:38:28 +0100

    Improve field error reporting
    Now if the parameter validation fails, a basic JSON structure will be returned

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Control.Lens hiding (view)
 import           Control.Monad.IO.Class (liftIO)
 import           Data.Aeson (encode, ToJSON)
+import           Data.Map (Map)
 import           Data.Text (Text)
 import           Snap.Core (route, writeLBS, setContentType, modifyResponse, MonadSnap)
 import           Snap.Http.Server (quickHttpServe)
 import           Text.Digestive (Method(Post), Form)
 import           Text.Digestive.Snap (method, defaultSnapFormConfig, runFormWith)
-import           Text.Digestive.View (childErrors)
+import           Text.Digestive.View (View, viewErrors)
 
+import qualified Data.Map as Map
+import qualified Data.Text as T
 import qualified MusicBrainz.API.Artist as Artist
 
 import           MusicBrainz (defaultConnectInfo, connectUser, connectDatabase, runMb, MusicBrainz)
@@ -22,10 +26,14 @@ expose f = do
     Just r -> do
       liftIO (runMbAction r) >>= writeLBS . encode
       modifyResponse (setContentType "application/json")
-    Nothing -> writeLBS (encode $ childErrors "" view)
+    Nothing -> writeLBS (encode $ errorMap view)
+
+errorMap :: View Text -> Map Text Text
+errorMap = Map.fromList . over (mapped._1) (T.intercalate ".") . viewErrors
 
 runMbAction :: MusicBrainz a -> IO a
 runMbAction = runMb defaultConnectInfo { connectDatabase = "musicbrainz_nes", connectUser = "musicbrainz" }
 
 main :: IO ()
-main = quickHttpServe $ route [("/artist/find-latest-by-mbid", expose Artist.findLatestByMbid)]
+main = quickHttpServe $ route [("/artist/find-latest-by-mbid", expose Artist.findLatestByMbid)
+                              ]
