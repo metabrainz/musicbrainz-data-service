3f6b0b2ae17043b27c5931a8c51ca3bbef2084d6
Oliver Charles <ollie@ocharles.org.uk>
2013-01-23 22:00:27 +0000

    Add open-session and close-session; require all actions to go through a session

--- src/MusicBrainz/Service.hs
+++ src/MusicBrainz/Service.hs
@@ -2,20 +2,25 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module MusicBrainz.Service (serviceInit, serviceInitContext) where
 
-import           Control.Applicative
+import           Control.Applicative ((<*>), (<$>))
+import           Control.Concurrent.STM (TVar, TMVar, atomically, newTVar, newTMVar, readTVar, takeTMVar, putTMVar, writeTVar, modifyTVar)
 import           Control.Exception (SomeException, try)
-import           Control.Monad
+import           Control.Monad (forM)
 import           Control.Monad.IO.Class (liftIO)
 import           Control.Monad.State.Class (gets)
 import           Data.Aeson (decode, encode, Value, object, (.=))
 import           Data.Configurator (lookupDefault)
+import           Data.Foldable (forM_)
+import           Data.Monoid (mempty)
 import           Data.Text (Text)
 import           Snap (Initializer, SnapletInit, makeSnaplet, Handler, getSnapletUserConfig, addRoutes)
-import           Snap.Core (writeLBS, setContentType, modifyResponse, setResponseCode, readRequestBody)
+import           Snap.Core (writeLBS, setContentType, modifyResponse, setResponseCode, readRequestBody, getsRequest, getHeader)
+import           Snap.Snaplet.Session.Common (mkCSRFToken, mkRNG, RNG)
 import           Text.Digestive (Form)
 import           Text.Digestive.Aeson (digestJSON, jsonErrors)
 
 import qualified Data.Map as Map
+import qualified Data.Text.Encoding as Encoding
 import qualified MusicBrainz.API.Artist as Artist
 import qualified MusicBrainz.API.ArtistType as ArtistType
 import qualified MusicBrainz.API.Edit as Edit
@@ -28,7 +33,7 @@ import qualified MusicBrainz.API.ReleaseGroup as ReleaseGroup
 import qualified MusicBrainz.API.Url as Url
 import qualified MusicBrainz.API.Work as Work
 
-import           MusicBrainz (defaultConnectInfo, connectUser, connectDatabase, connectPassword, MusicBrainz, Context, openContext, runMbContext, withTransaction)
+import           MusicBrainz (defaultConnectInfo, connectUser, connectDatabase, connectPassword, MusicBrainz, Context, openContext, runMbContext, begin, ConnectInfo(..), commit)
 import           MusicBrainz.API.JSON (TopLevel)
 
 --------------------------------------------------------------------------------
@@ -48,8 +53,18 @@ expose f = do
 
       -- We run the 'form' that validates the users submitted parameters to the API
       -- call.
-      context <- gets svcContext
-      outcome <- liftIO (try (runMbContext context (withTransaction $ digestJSON f json')))
+      Just token <- fmap (fmap Encoding.decodeUtf8) $ getsRequest (getHeader "MB-Session")
+      sessionStore <- gets openSessions
+      context <- liftIO $ atomically $ do
+        ss <- readTVar sessionStore
+        takeTMVar (ss Map.! token)
+
+      outcome <- liftIO (try (runMbContext context (digestJSON f json')))
+
+      liftIO $ atomically $ do
+        ss <- readTVar sessionStore
+        putTMVar (ss Map.! token) context
+
       modifyResponse (setContentType "application/json")
       case outcome of
         Left (exception :: SomeException) -> do
@@ -72,8 +87,53 @@ expose f = do
 
 
 --------------------------------------------------------------------------------
-data Service = Service { svcContext :: Context }
+type SessionToken = Text
 
+
+--------------------------------------------------------------------------------
+data Service = Service { connectInfo :: ConnectInfo
+                       , openSessions :: TVar (Map.Map SessionToken (TMVar Context))
+                       , rng :: RNG
+                       }
+
+
+--------------------------------------------------------------------------------
+openSession :: Handler Service Service ()
+openSession = do
+  -- TODO Handle collisions
+  token <- gets rng >>= liftIO . mkCSRFToken
+
+  sessionStore <- gets openSessions
+  context <- gets connectInfo >>= openContext
+
+  runMbContext context begin
+
+  liftIO $ atomically $ do
+    s <- newTMVar context
+    modifyTVar sessionStore (Map.insert token s)
+
+  writeLBS . encode $ object [ "token" .= token ]
+
+
+--------------------------------------------------------------------------------
+closeSession :: Handler Service Service ()
+closeSession = do
+  Just token <- fmap (fmap Encoding.decodeUtf8) $ getsRequest (getHeader "MB-Session")
+
+  sessionStore <- gets openSessions
+
+  context <- liftIO $ atomically $ do
+    allSessions <- readTVar sessionStore
+    case Map.lookup token allSessions of
+      Just session -> do
+        writeTVar sessionStore (Map.delete token allSessions)
+        Just <$> takeTMVar session
+      Nothing -> return Nothing
+
+  forM_ context $ \c -> runMbContext c (MusicBrainz.commit)
+
+
+--------------------------------------------------------------------------------
 serviceInit :: SnapletInit Service Service
 serviceInit = serviceInitContext $ do
   config <- getSnapletUserConfig
@@ -82,16 +142,19 @@ serviceInit = serviceInitContext $ do
     , ("username", connectUser)
     , ("password", connectPassword)
     ] $ \(k, def) -> lookupDefault (def defaultConnectInfo) config k
-  liftIO (openContext defaultConnectInfo
-    { connectDatabase = db
-    , connectUser = user
-    , connectPassword = pass
-    })
+  return $ defaultConnectInfo { connectDatabase = db
+                              , connectUser = user
+                              , connectPassword = pass
+                              }
 
-serviceInitContext :: Initializer Service Service Context -> SnapletInit Service Service
+--------------------------------------------------------------------------------
+serviceInitContext :: Initializer Service Service ConnectInfo -> SnapletInit Service Service
 serviceInitContext ctxInit = makeSnaplet "service" "musicbrainz-data HTTP service" Nothing $ do
   addRoutes
-    [ ("/artist/create", expose Artist.create)
+    [ ("/open-session", openSession)
+    , ("/close-session", closeSession)
+
+    , ("/artist/create", expose Artist.create)
     , ("/artist/find-latest", expose Artist.findLatest)
     , ("/artist/view-revision", expose Artist.viewRevision)
 
@@ -130,3 +193,5 @@ serviceInitContext ctxInit = makeSnaplet "service" "musicbrainz-data HTTP servic
     ]
 
   Service <$> ctxInit
+          <*> liftIO (atomically $ newTVar mempty)
+          <*> liftIO mkRNG
